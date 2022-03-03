43e203393c86da22a688310eca0df4406a9b26d8
William Little <willbythesea@gmail.com>
2022-03-03 11:56:40 -0500

    Initial Commit

---
+++ iConsEquipmentRental/iConsEquipmentRentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConsEquipmentRentalApp.swift
+//  iConsEquipmentRental
+//
+//  Created by William Little on 2022-03-03.
+//
+
+import SwiftUI
+
+@main
+struct iConsEquipmentRentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
